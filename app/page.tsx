739ae4c1219a65a3541a37f2e49c3c879a02d8fe
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-12 02:35:29 +0300

    adding new button sing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
-
+const font = Poppins({
+  subsets: ["latin"],
+  weight:["600"]
+})
 export default function Home() {
   return (
    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-    <div className="space-y-6">
-       <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+    <div className="space-y-6 text-center">
+       <h1 className={cn(
+          "text-7xl font-semibold text-white drop-shadow-md",
+          font.className
+          
+          )}>
        🔐 Auth
         </h1>
         <p className="text-white text-lg">
           A simple Authentication Service
         </p>
+
+        <Button>
+          
+          </Button>
+
+
     </div>
 
    </main>
