e0a84f89f14fa917c85cddee23a8a4ba1f210fd1
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-12 02:26:35 +0300

    Featr: Prepering our Landing Page with NextAuth

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="flex h-full flex-col items-center justify-center">
+   <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="space-y-6">
+       <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+       🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple Authentication Service
+        </p>
+    </div>
 
    </main>
   );
