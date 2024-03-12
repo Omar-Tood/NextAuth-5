378a9a69b06e305286d7f11b8dafd1663882a331
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-12 03:26:00 +0300

    finishing our layout page on Auth

--- app/Auth/layout.tsx
+++ app/Auth/layout.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const AutLayout = () => {
-  return (
-    <div>AutL</div>
-  )
-}
-
-export default AutLayout
+const AuthLayout = ({ 
+    children
+  }: { 
+    children: React.ReactNode
+  }) => {
+    return ( 
+      <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+        {children}
+      </div>
+     );
+  }
+   
+  export default AuthLayout;
