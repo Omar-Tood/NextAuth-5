f6949509fcfcbcc7466dfdc5e2011f3e9868e511
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-12 03:35:07 +0300

    fixed: LogiForm Page

--- app/Auth/login/page.tsx
+++ app/Auth/login/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import { LoginForm } from '@/components/Auth/login-form'
 const LoginPage = () => {
   return (
-    <div>
-        Login Page
-    </div>
+    <LoginForm/>
   )
 }
 
---
+++ components/Auth/login-form.tsx
@@ -0,0 +1,7 @@
+export const LoginForm = () =>{
+    return(
+        <div>
+            LoginForm
+        </div>
+    )
+}
