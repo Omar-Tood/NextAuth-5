12fb561d5fb26f45cbda00ad2fb95815214a78f8
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-13 14:38:10 +0300

    setup Google account

--- components/Auth/social.tsx
+++ components/Auth/social.tsx
@@ -1,9 +1,18 @@
 'use client'
-
+import { FcGoogle } from "react-icons/fc";
+import {FaGithub} from "react-icons/fa";
+import { Button } from "../ui/button";
 export const Social = ()=>{
     return(
         <div className="flex items-center w-full gap-x-2">
-
+          <Button
+           size="lg"
+           className="w-full"
+           variant="outline"
+           onClick={()=> {}}
+          >
+            <FcGoogle />
+          </Button>
         </div>
     )
 }
