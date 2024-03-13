5642619c78e72e801931bcdf3ebcdb5fce3e20e1
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-13 15:19:39 +0300

    add functionally zod

--- components/Auth/login-form.tsx
+++ components/Auth/login-form.tsx
@@ -1,6 +1,22 @@
+import * as z from  "zod"
+import {useForm} from  'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { CardWrapper } from "./card-wrapper"
+import { LoginSchema } from "@/schemas";
+import {
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormMessage
+
+} from "@/components/ui/form"
 
 export const LoginForm = () =>{
+     const  form = useForm<z.infer<typeof LoginSchema>>({
+        
+     })
+
     return(
         <CardWrapper
         headerLabel="Welcome Back✋🏾"
