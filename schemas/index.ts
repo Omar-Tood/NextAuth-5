c6c45fddda27e33f89dd78c7ba63ee75c40063bd
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-14 16:15:29 +0300

    add email is required

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,8 @@
 import * as z from "zod"
 
 export const LoginSchema =  z.object({
-    email: z.string().email(),
+    email: z.string().email(({
+        message: "Email is required"
+    })),
     password: z.string(),
 })
