665d6b8331231d0210347f717a53895b3a7fc31f
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-14 16:44:14 +0300

    feat: Fixed possword is required

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,5 +4,7 @@ export const LoginSchema =  z.object({
     email: z.string().email(({
         message: "Email is required"
     })),
-    password: z.string(),
+    password: z.string().min(1, {
+        message: "Password is required"
+    })
 })
