b3065d62a89cdada774d9e592dbc7b57dffbb38b
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-14 16:07:59 +0300

    feat: Adding Input Field

--- components/Auth/login-form.tsx
+++ components/Auth/login-form.tsx
@@ -4,11 +4,13 @@ import {useForm} from  'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CardWrapper } from "./card-wrapper"
 import { LoginSchema } from "@/schemas";
+import { Input } from "../ui/input";
 import {
    Form,
    FormControl,
    FormField,
    FormItem,
+   FormLabel,
    FormMessage
 
 } from "@/components/ui/form"
@@ -35,13 +37,28 @@ export const LoginForm = () =>{
             <form onSubmit={form.handleSubmit(()=>{})}
             className="space-y-6"
             >
+                <div className="space-y-4">
+                    <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) =>(
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input/>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                    />
+
+                    
+                </div>
 
             </form>
 
             
          </Form>
      
-             LoginForm
         </CardWrapper>
            
        
