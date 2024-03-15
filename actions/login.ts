71d5bf87b0babec36f7d7c88f253d29a9fddc426
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-16 02:45:14 +0300

    validate login server acctions

--- actions/login.ts
+++ actions/login.ts
@@ -1,5 +1,6 @@
 'use server'
-
-export const login = (values: any)=>{
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+export const login = (values: z.infer<typeof LoginSchema>)=>{
     console.log(values)
 };
