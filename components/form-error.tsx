1e52be22466ea73ae38bba182345dfcddac45c27
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-16 01:43:16 +0300

    Solving Same at Form Error adding new package

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -12,8 +12,9 @@ export const FormError = ({
     return(
         <div className="bg-destructive/15 p-3 rounded-sm flex items-center gap-x-2 text-sm text-destructive">
 
-            <ExclamationTriangleIcon/>
+            <ExclamationTriangleIcon className="h-4 w-4"/>
+            <p>{message}</p>
 
         </div>
-    )
-}
+    );
+};
