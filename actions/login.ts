8088753b09aecbeb0946de24752d74d62f094c82
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-03-16 02:49:56 +0300

    Login Schema Validate on the Server Acctions

--- actions/login.ts
+++ actions/login.ts
@@ -2,5 +2,12 @@
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
 export const login = (values: z.infer<typeof LoginSchema>)=>{
-    console.log(values)
+    // console.log(values)
+    const validateFields = LoginSchema.safeParse(values);
+    if(!validateFields.success){
+        return {error: "Invalid Fields"};
+    };
+   
+    return {success: "Email Sent!"}
+
 };
