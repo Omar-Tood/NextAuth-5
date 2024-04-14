bca8f190777c6856fb7d3c75a0d27f1ac3606d08
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-04-14 23:58:25 +0300

    i got error neon url database

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+
+  
 }>) {
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
