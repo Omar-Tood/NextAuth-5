0f12d9974923606497cb92ed3baf51733cdc0958
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-04-14 22:21:36 +0300

    create prisma cleint

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,8 @@
 import {PrismaClient} from '@prisma/client'
 
-export 
+declare global{
+    var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+if(process.env.NODE_ENV === 'production') globalThis.prisma = db
