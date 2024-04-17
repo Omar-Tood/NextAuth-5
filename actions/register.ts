8701efb978e47db5cef8de598334950546237866
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-04-17 12:51:39 +0300

    complated our register form validation password hashing ..

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 'use server'
 import * as z from "zod";
 import bcrypt from 'bcrypt'
+import { db } from "@/lib/db";
 import { RegisterScheme } from "@/schemas";
+import { error } from "console";
 export const register = async(values: z.infer<typeof RegisterScheme>)=>{
     // console.log(values)
     const validateFields = RegisterScheme.safeParse(values);
@@ -11,6 +13,26 @@ export const register = async(values: z.infer<typeof RegisterScheme>)=>{
 
     const {email, password, name} = validateFields.data;
     const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const existingUSer = await db.user.findUnique({
+        where:{
+            email,
+        }
+    });
+
+   if(existingUSer){
+    return {error: "User already exists!"}
+   }
+   
+   await db.user.create({
+    data:{
+        name,
+        email,
+        password: hashedPassword
+    }
+   });
+  
+
    
     return {success: "Email Sent!"}
 
