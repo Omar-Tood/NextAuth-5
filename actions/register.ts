c2246afac28352fc9b5bf805cfd320835aa368f2
Omar Jibirl Abdullkadir <129175570+Omar-Tood@users.noreply.github.com>
2024-04-17 12:42:14 +0300

    hashPassword created

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,6 @@
 'use server'
 import * as z from "zod";
+import bcrypt from 'bcrypt'
 import { RegisterScheme } from "@/schemas";
 export const register = async(values: z.infer<typeof RegisterScheme>)=>{
     // console.log(values)
@@ -7,6 +8,9 @@ export const register = async(values: z.infer<typeof RegisterScheme>)=>{
     if(!validateFields.success){
         return {error: "Invalid Fields"};
     };
+
+    const {email, password, name} = validateFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
    
     return {success: "Email Sent!"}
 
